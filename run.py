22232e68ef27bca5f5ff53ced39048ba14c0ad8b
Marko Burjek <s0mebody.slo@gmail.com>
2013-03-16 07:07:23 -0400

    Tested log extraTree

--- run.py
+++ run.py
@@ -18,6 +18,10 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.cross_validation import cross_val_score
 
 
+def log_mean_absolute_error(y_true, y_pred):
+    return mean_absolute_error(np.exp(y_true), np.exp(y_pred))
+
+
 paths = get_paths("Settings.json")
 data_dir = paths["data_path"]
 cache_dir = path_join(data_dir, "tmp")
@@ -46,6 +50,7 @@ print "valid features", validation_features.shape
 
 salaries = np.array(list(read_column(paths["train_data_path"], "SalaryNormalized"))).astype(np.float64)
 valid_salaries = np.array(list(read_column(paths["valid_data_path"], "SalaryNormalized"))).astype(np.float64)
+salaries = np.log(salaries)
 print salaries.shape
 #classifier = RandomForestRegressor(n_estimators=10,
                                    #verbose=2,
@@ -56,7 +61,7 @@ print salaries.shape
 for n_trees in range(10,21,10):
     for min_samples_split in [2, 30]:
         print n_trees
-        name = "ExtraTree_min_sample%d_%dtrees_100f_categoryTimeType_tfidf" % (min_samples_split, n_trees)
+        name = "ExtraTree_min_sample%d_%dtrees_100f_categoryTimeType_tfidf_log" % (min_samples_split, n_trees)
         print name
         classifier = ExtraTreesRegressor(n_estimators=n_trees,
                                         verbose=2,
@@ -71,7 +76,7 @@ for n_trees in range(10,21,10):
         predictions = classifier.predict(validation_features)
         print valid_salaries[1:10]
         print predictions[1:10]
-        mae = mean_absolute_error(valid_salaries, predictions)
+        mae = mean_absolute_error(valid_salaries, np.exp(predictions))
         print "MAE validation: ", mae
         save_model(classifier, name, mae)
         #oob_predictions = classifier.oob_prediction_
@@ -83,7 +88,7 @@ for n_trees in range(10,21,10):
                                         oob_score=False,
                                         min_samples_split=min_samples_split,
                                         random_state=3465343)
-        scores = cross_val_score(classifier, features, salaries, cv=3, score_func=mean_absolute_error, verbose=1)
+        scores = cross_val_score(classifier, features, salaries, cv=3, score_func=log_mean_absolute_error, verbose=1)
         print "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
         mae_cv = "%0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
         save_model(classifier, name, mae, mae_cv)
