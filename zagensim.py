b0373d876db040faf617c43141bebbd48aa46208
Marko Burjek <s0mebody.slo@gmail.com>
2013-03-23 23:39:13 +0100

    Played with gensim
    TODO: remove those with 1 showing.
    Do PCA or something to get 500 features

--- zagensim.py
+++ zagensim.py
@@ -7,23 +7,19 @@ from data_io import (
     #label_encode_column_transform
 )
 from os.path import join as path_join
-#import joblib
-#import numpy as np
-#from sklearn.ensemble import RandomForestRegressor
-#import joblib
+import re
 
-#from gensim.corpora import TextCorpus
-#from gensim.corpora import MmCorpus
-#import re
-import string
+#import string
 import logging
-#from nltk.tokenize.regexp import WordPunctTokenizer
+from nltk.tokenize.regexp import WordPunctTokenizer
 from nltk.corpus import stopwords
-#from itertools import izip, repeat
+from itertools import izip, repeat
 import operator
+import joblib
+from collections import Counter
 
 from gensim.corpora.textcorpus import TextCorpus
-#from gensim.corpora.dictionary import Dictionary
+from gensim.corpora.dictionary import Dictionary
 from gensim.corpora.mmcorpus import MmCorpus
 paths = get_paths("Settings_loc5.json")
 data_dir = paths["data_path"]
@@ -33,16 +29,37 @@ tmp_dir = path_join(data_dir, "tmp")
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 logger = logging.getLogger('test_miislita')
 
+regex = re.compile("^(.+;\s*)\n", re.MULTILINE)
+regex_num = re.compile("\W(\d+)\W")
+regex_hex = re.compile("\W([a-f0-9-]+)\W")
+regex_punct = re.compile("^([:/\]\[\)\(,\.\}\{\?\!#=@'0-9]+)$")
+english_stopwords = stopwords.words('english')
+english_stopwords = dict(izip(english_stopwords, repeat(True, len(english_stopwords))))
+#tokenizer = WordPunctTokenizer()
+
+#cnt = Counter()
+
+
+def word_ok(word):
+    if word in english_stopwords:
+        return False
+    if regex_punct.match(word):
+        return False
+    #cnt[word] += 1
+    return True
+
+
 class Corpus_Column(TextCorpus):
-    stoplist = set('for a of the and to in on'.split()).union(set(string.punctuation))
+    #stoplist = set('for a of the and to in on'.split()).union(set(string.punctuation))
 
     def __init__(self, input=None, column=None):
-        super(Corpus_Column, self).__init__(input)
         self.column = column
+        #self.input = input
+        super(Corpus_Column, self).__init__(input)
 
     def getstream(self):
         logger.info("getting stream")
-        reader = read_column(self.input, "Title")
+        reader = read_column(self.input, self.column)
         return reader
 
     def get_texts(self):
@@ -52,15 +69,15 @@ class Corpus_Column(TextCorpus):
 
         .cor format: one document per line, words separated by whitespace.
         """
-        #tokenizer = WordPunctTokenizer()
+        tokenizer = WordPunctTokenizer()
         #print CorpusCSV.stoplist
         #return self.getstream()
         for doc in self.getstream():
-            yield [word for word in doc.lower().split()]
+            #yield [word for word in doc.lower().split()]
                     #if word not in CorpusMiislita.stoplist]
             #yield doc
-            #yield [word for word in tokenizer.tokenize(clean_me(doc[7], doc[6]).lower())
-                    #if word_ok(word)]
+            yield [word for word in tokenizer.tokenize(doc.lower())
+                   if word_ok(word)]
 
     def __len__(self):
         """Define this so we can use `len(corpus)`"""
@@ -70,25 +87,48 @@ class Corpus_Column(TextCorpus):
         return self.length
 
 
-stoplist = set('for a of the and to in'.split())
-stoplist = stopwords.words('english')
+#stoplist = set('for a of the and to in'.split())
+#stoplist = stopwords.words('english')
 
 #dictionary = Dictionary(title.lower().split() for title in read_column(paths["train_data_path"], "Title"))
 #print dictionary
 
 fname = paths["train_data_path"]
 
-title_corpus = Corpus_Column(fname, "Title")
-MmCorpus.serialize(path_join(cache_dir, "train_title_raw_corpus.pickle"), title_corpus)
-title_corpus.dictionary.save(path_join(cache_dir, "train_title_raw_dic.pickle"))
-print title_corpus
-print title_corpus.dictionary
+
+#title_corpus = Corpus_Column(fname, "Title")
+#MmCorpus.serialize(path_join(cache_dir, "train_title_nltk_corpus.pickle"), title_corpus)
+#title_corpus.dictionary.save(path_join(cache_dir, "train_title_nltk_dic.pickle"))
+#print title_corpus
+#print title_corpus.dictionary
+
+#description_corpus = Corpus_Column(fname, "FullDescription")
+#print len(description_corpus)
+#for word in description_corpus.get_texts():
+    #a = 5
+#joblib.dump(cnt, path_join(cache_dir, "counter_train_desc_nltk"), compress=3)
+
+cnt = joblib.load(path_join(cache_dir, "counter_train_desc_nltk"))
+
+for word, freq in cnt.most_common(10): #[:-100:-1]:
+    print word, freq
+#MmCorpus.serialize(path_join(cache_dir, "train_desc_nltk_corpus.pickle1"), description_corpus)
+#description_corpus.dictionary.save(path_join(cache_dir, "train_desc_nltk_dic.pickle"))
+dicti = Dictionary.load(path_join(cache_dir, "train_desc_nltk_dic.pickle"))
+#dicti = description_corpus.dictionary
+print dicti
+#print description_corpus
+#print description_corpus.dictionary
 #print files.dictionary
-id2token = title_corpus.dictionary.values()
+
+#id2token = dicti.id2token
 i = 0
-for k, v in sorted(title_corpus.dictionary.dfs.iteritems(), key=operator.itemgetter(1), reverse=True):
-    if i < 100:
-        print id2token[k], v
+for k, v in sorted(dicti.dfs.items(), key=operator.itemgetter(1), reverse=True):
+    if i < 10:
+        print dicti[k], v, "ID:", k
         i = i + 1
-
-
+k=0
+print "printam token", k
+print id2token[k], dicti.dfs[k], "ID:", k
+#def num_appear(id):
+    #total_sum = sum(
